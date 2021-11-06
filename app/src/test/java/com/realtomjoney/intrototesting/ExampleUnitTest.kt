e66e892292d0ac451a9d4b5952153dc8f2197049
xTheCoderx <justaguyoney@gmail.com>
2021-11-06 18:03:16 +1300

    First test, asserting that 'activity.readValue()' returns null.

--- app/src/test/java/com/realtomjoney/intrototesting/ExampleUnitTest.kt
+++ app/src/test/java/com/realtomjoney/intrototesting/ExampleUnitTest.kt
@@ -1,25 +1,26 @@
 package com.realtomjoney.intrototesting
 
+import androidx.test.core.app.ActivityScenario
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+@RunWith(AndroidJUnit4::class)
+@Config(sdk = [29])
+
 class ExampleUnitTest {
     @Test
-    fun stringIsFalse() {
-        val myMain = MainActivity()
-        val myStr = myMain.readValue()
-
-        assertNull(myStr)
-    }
-
-    @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun first_test() {
+        ActivityScenario.launch(MainActivity::class.java).onActivity { activity ->
+            assert(activity.readValue() == null)
+        }
     }
 }
